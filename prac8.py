210975e4a3449b0d24f5a2cc6e125785b5f6b1d2
D21CE174 <97745319+D21CE174@users.noreply.github.com>
2022-03-25 18:55:32 +0530

    Add files via upload

---
+++ prac8.py
@@ -0,0 +1,15 @@
+stack=[]
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print ('install stack')
+print (stack)
+print ('\n element pop from stack')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print ('\n stack after element are poped:')
+print(stack)
+
+print("this program is prepared by darshan and id : d21ce174")  
+
